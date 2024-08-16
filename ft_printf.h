221258e1c9c28b9ae447b9784df3076fa406934f
Undimmer <145223362+undimmer@users.noreply.github.com>
2024-08-16 10:09:36 +0200

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -1,30 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                          :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anquesad <anquesad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/22 14:34:47 by anquesad          #+#    #+#             */
-/*   Updated: 2024/07/22 16:16:03 by anquesad         ###   ########.fr       */
+/*   Created: 2024/08/11 15:18:39 by anquesad          #+#    #+#             */
+/*   Updated: 2024/08/11 18:24:08 by anquesad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdarg.h>
-#include <unistd.h>
+# include <stdarg.h>
+# include <unistd.h>
 
-# define HEXA_MIN "0123456789abcdef"
-# define HEXA_MAY "0123456789ABCDEF"
+# define HEXA_MIN "0123456789abdef"
+# define HEXA_MAY "01236456789ABDEF"
 # define DECIMAL "0123456789"
 
-int     ft_printf(char const *str, ...);
-int     ft_strlen(const char *str);
-void    ft_pf_putchar(char c, int *resultado);
-void    ft_pf_putstr(char *s, int *resultado);
-void    ft_pf_pointer(unsigned long long ptr, int *resultado);
-void    ft_pf_putnbr_base(long long nbr, char *base, int *resultado);
+int                     ft_pf_putchar(int c);
+int                     ft_pf_putstr(char *s);
+int                     ft_pf_putnbr_base(int n);
+int                     ft_pf_putnbr_base_u(unsigned n);
+int                     ft_pf_pointer_zero(unsigned long int n);
+int                     ft_printf(char const *str, ...);
+int                     select_format(va_list arg, char c);
 
 #endif
