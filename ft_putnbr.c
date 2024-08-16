26090b20725f049cb96ca03d2c9fe5b909ed0895
Undimmer <145223362+undimmer@users.noreply.github.com>
2024-08-16 10:08:27 +0200

    Create ft_putnbr.c

---
+++ ft_putnbr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anquesad <anquesad@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/11 18:32:55 by anquesad          #+#    #+#             */
+/*   Updated: 2024/08/11 18:32:59 by anquesad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int     ft_pf_putnbr_base(int n)
+{
+        int                     i;
+        long int        nbr;
+
+        i = 0;
+        nbr = n;
+        if (nbr < 0)
+        {
+                i += ft_pf_putchar('-');
+                nbr = -nbr;
+        }
+        if (nbr / 10)
+        {
+                i += ft_pf_putnbr_base(nbr / 10);
+        }
+        i += ft_pf_putchar(nbr % 10 + '0');
+        return (i);
+
+}
+
+int     ft_pf_putnbr_base_u(unsigned n)
+{
+        unsigned        i;
+
+        i = 0;
+
+        if (nbr / 10)
+        {
+                i += ft_pf_putnbr_base_u(nbr / 10);
+        }
+        i += ft_pf_putchar(nbr % 10 + '0');
+        return (i);
+}
